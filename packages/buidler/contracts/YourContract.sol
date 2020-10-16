ff1219423a2f70225903ba03a25a2857ef0552ed
captnseagraves <k.s.seagraves@gmail.com>
2020-10-15 21:50:18 -0700

    Playing with scaffold-eth

--- packages/buidler/contracts/YourContract.sol
+++ packages/buidler/contracts/YourContract.sol
@@ -4,14 +4,35 @@ import "@nomiclabs/buidler/console.sol";
 
 contract YourContract {
 
+  uint8 public count = 1;
+  uint256 public timeOfDeploy = block.timestamp;
+  address public owner;
+
+  constructor () public {
+    owner = 0x90D4CeD092C11a6Af9D352Bf3648Bb55288fd113;
+  }
+
+  function dec() public {
+    count = count - 1;
+  }
+
   event SetPurpose(address sender, string purpose);
+  event ChangeOwner(address sender, address owner, address newOwner);
 
   string public purpose = "🛠 Programming Unstoppable Money";
 
   function setPurpose(string memory newPurpose) public {
+    require(msg.sender == owner);
+
     purpose = newPurpose;
     console.log(msg.sender,"set purpose to",purpose);
     emit SetPurpose(msg.sender, purpose);
+  }
+
+  function changeOwner(address newOwner) public {
+    owner = newOwner;
+    console.log(msg.sender,"changed owner to",owner,newOwner);
+    emit ChangeOwner(msg.sender, owner, newOwner);
   }
 
 }
